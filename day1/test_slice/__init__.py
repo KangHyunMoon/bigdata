8a9b71918458d527fd44967659d00f98af547883
KangHyunMoon <bjedpr1@naver.com>
2019-09-19 15:19:33 +0900

    commit ..

---
+++ day1/test_slice/__init__.py
@@ -0,0 +1,14 @@
+from pandas import Series
+
+data4 = Series([15,53,24,52,93,30,33,80,90,77],
+                index = list('abcdefghij'))
+
+print(data4[-3:])
+
+print(data4[-3:-1])
+
+print(data4['c':'f'])
+
+print(data4 > 60)
+
+print(data4[data4>60])
